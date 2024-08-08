e47c1090ad897d7a2e767576fbf9b1c375a45a67
Legend Andrews <139176918+Cantrell24@users.noreply.github.com>
2024-08-08 15:59:21 +0000

    First commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,12 @@ import '../../styles/home.css'
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-black mb-3">
-			<Link to="/home">
+			<Link to="/">
 				<button className="btn btn-warning">Home</button>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/favorites">
-					<button className="btn btn-primary">Favorites</button>
+					<button className="btn btn-danger">Favorites</button>
 				</Link>
 			</div>
 		</nav>
